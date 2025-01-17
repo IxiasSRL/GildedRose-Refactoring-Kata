d78bbe92dc8e8fb2acc61ddbec6f412bb37c5176
aayadiaa <akrem.ayadi@minfin.fed.be>
2025-01-17 14:12:58 +0100

    Solution Akrem AYADI

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.function.Consumer;
+
 class GildedRose {
     Item[] items;
 
@@ -8,54 +10,65 @@ class GildedRose {
     }
 
     public void updateQuality() {
+
+        Consumer<Item> increaseQuality = (item) -> { if(item.quality < 50){item.quality ++; }};
+        Consumer<Item> decreaseQuality = (item) -> { if(item.quality >0 ){item.quality --; }};
+        Consumer<Item> updateNormalItem = (item) -> {
+            decreaseQuality.accept(item);
+            if(item.sellIn <=0){
+                decreaseQuality.accept(item);
+            }
+        };
+
+        Consumer<Item> updateAgedBrieItem = (item) -> {
+            increaseQuality.accept(item);
+            if(item.sellIn <=0){
+                increaseQuality.accept(item);
+            }
+        };
+
+        Consumer<Item>  updateBackstagePassItem = (item) -> {
+            if(item.sellIn > 10){
+                increaseQuality.accept(item);
+            }else if(item.sellIn > 5){
+                increaseQuality.accept(item);
+                increaseQuality.accept(item);
+            }else if(item.sellIn > 0){
+                increaseQuality.accept(item);
+                increaseQuality.accept(item);
+                increaseQuality.accept(item);
+            }else {
+                item.quality =0 ;
+            }
+
+        };
+        Consumer<Item>  updateSulfurasItem = (item) -> {};
+        Consumer<Item>  updateConjuredItem = (item) -> {
+            decreaseQuality.accept(item);
+            decreaseQuality.accept(item);
+            if(item.sellIn <=0){
+                decreaseQuality.accept(item);
+                decreaseQuality.accept(item);
+            }
+
+        };
+        boolean updateSellIn = true ;
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            updateSellIn = true ;
+            if(items[i].name.equals("Aged Brie")){
+                updateAgedBrieItem.accept(items[i]);
+            }else if(items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                updateSellIn = false ;
+                updateSulfurasItem.accept(items[i]);
+            }else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                updateBackstagePassItem.accept(items[i]);
+            }else if(items[i].name.startsWith("Conjured")){
+                updateConjuredItem.accept(items[i]);
+            }else {
+                updateNormalItem.accept(items[i]);
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if(updateSellIn) {
+                items[i].sellIn--;
             }
         }
     }
